fb05f8f1e371e1fd8cd915fb15b5c7ad51b02e54
Tim Dorr <timdorr@asmallorange.com>
2009-05-02 01:37:58 -0400

    Adding in a sample config file

---
+++ app/Config.sample.php
@@ -0,0 +1,14 @@
+<?php
+
+$CONFIG['db_type'] = 'MySQL';
+$CONFIG['db_host'] = 'localhost';
+$CONFIG['db_user'] = 'user';
+$CONFIG['db_pass'] = 'password';
+$CONFIG['db_name'] = 'asoworx';
+
+$CONFIG['session_type'] = "Db";
+$CONFIG['session_timeout'] = 3600;
+$CONFIG['session_domain'] = '';
+$CONFIG['session_path'] = '/';
+
+$CONFIG['display_backend'] = "Smarty";
